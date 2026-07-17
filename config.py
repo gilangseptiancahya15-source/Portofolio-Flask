fb6b8c83dfc5829ba46a17af743faac29732dcf2
Gilang Septian <gilangseptiancahya15@gmail.com>
2026-07-17 21:16:22 +0700

    konfigurasi postgreSQL supabase untuk deploy vercel

--- config.py
+++ config.py
@@ -2,17 +2,21 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
-   
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'kunci-rahasia-portfolio-flask-2026')
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "kunci-rahasia-portfolio-flask-2026"
+    )
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'portfolio.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(BASE_DIR, "portfolio.db")
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, "static", "uploads")
 
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
+    ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "webp"}
 
-    MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 2 MB
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
